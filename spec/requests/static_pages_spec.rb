96ec5cdc72dc66be552996a5f6e80faacc91123b
Kaleem Ahned <drkaleem@gmail.com>
2014-05-31 15:32:36 -0400

    Finish Static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  describe "Home Page" do
+    it "should have content 'Sample App'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      #get static_pages_index_path
+      #response.status.should be(200)
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end    
+  end
+
+  describe "Help Page" do
+    it "should have content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    end
+  end
+
+  describe "About Page" do
+    it "should have content 'About '" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+  it "should have the right title" do
+    visit '/static_pages/about'
+    expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+  end    
+  end
+end
